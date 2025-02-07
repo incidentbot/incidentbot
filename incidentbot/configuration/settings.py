48add983a778421b14baeb899cbf8691337b9e8d
Scott Hawkins <scott@echoboomer.net>
2025-02-06 21:50:11 -0500

    Release v2.1.3 - Bugfix for group auto invites. (#668)

--- incidentbot/configuration/settings.py
+++ incidentbot/configuration/settings.py
@@ -18,7 +18,7 @@ from pydantic_settings import (
 from typing import Annotated, Any, Tuple, Type
 from typing_extensions import Self
 
-__version__ = "v2.1.2"
+__version__ = "v2.1.3"
 
 opsgenie_logo_url = "https://i.imgur.com/NjiEBCu.png"
 pagerduty_logo_url = "https://i.imgur.com/IVvdFCV.png"
